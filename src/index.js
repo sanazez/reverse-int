37e3ec6a272387a760b2f4db03b02d591cfb97c2
sanazez <sanazez1234@gmail.com>
2020-10-03 21:29:18 +0300

    feat: added solution this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let str = n + '';
+    if (str.includes('-')) {
+        return Number(str.slice(1).split('').reverse().join(''));
+    } else {
+        let arr = str.split('');
+        return Number(arr.reverse().join(''));
+    }
 }
